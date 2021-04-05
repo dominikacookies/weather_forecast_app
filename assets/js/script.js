522e450a354bdd43b252c823a45fb002af776d77
Dominika <domi_nika@hotmail.co.uk>
2021-04-05 21:57:08 +0100

    app successfuly retrieves input value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
+function getCityName () {
+  currentTarget.sibling("input").val();
+}
+
 function searchForCityWeather (event) {
   event.preventDefault();
-  
+  // retrieve form input
+  let cityName = $(event.currentTarget).siblings("input").val();
+  console.log(cityName);
+  // fetch weather data
 }
 
 $("#searchForCityWeather").on( "click", "button", searchForCityWeather)
