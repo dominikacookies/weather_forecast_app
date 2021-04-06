00ac6f4005e76845cec1d699f0c2f55c5344d05d
Dominika <domi_nika@hotmail.co.uk>
2021-04-06 15:18:59 +0100

    somewhatfixed html by implementing row class. Also, fetch url accepts cityName varaiable and retrieves data

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,24 @@ function getCityName () {
   currentTarget.sibling("input").val();
 }
 
+function fetchWeatherData (cityName) {
+  console.log(cityName)
+  let weatherApiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=f1fdda4864afff5226ddcc1a17f0350f`;
+    
+  const functionForJSON = (responseObject) => {
+      console.log(responseObject)
+    };
+
+  fetch(weatherApiUrl)
+  .then(functionForJSON)
+}
+
 function searchForCityWeather (event) {
   event.preventDefault();
   // retrieve form input
   let cityName = $(event.currentTarget).siblings("input").val();
-  console.log(cityName);
   // fetch weather data
+  fetchWeatherData(cityName);
 }
 
 $("#searchForCityWeather").on( "click", "button", searchForCityWeather)
