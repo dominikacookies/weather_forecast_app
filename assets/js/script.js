af5008c9cf0a0b82c21417d97d11fb21ac277e39
Dominika <domi_nika@hotmail.co.uk>
2021-04-06 16:12:39 +0100

    lat lon info succesfully retrieved from API and used to build the url for next API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,24 @@ function getCityName () {
 }
 
 function fetchWeatherData (cityName) {
-  console.log(cityName)
-  let weatherApiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=f1fdda4864afff5226ddcc1a17f0350f`;
-    
+  let weatherApiUrlForLonLat = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=f1fdda4864afff5226ddcc1a17f0350f`;
+  
   const functionForJSON = (responseObject) => {
-      console.log(responseObject)
-    };
+    return responseObject.json();
+  };
 
-  fetch(weatherApiUrl)
-  .then(functionForJSON)
+  const functionForApplication = (dataFromServer) => {
+    const lonLatObject = {
+      lon: dataFromServer.coord.lon,
+      lat: dataFromServer.coord.lat,
+    };
+    weatherApiUrlForWeatherInfo = `https://api.openweathermap.org/data/2.5/onecall?lat=${lonLatObject.lat}&lon=${lonLatObject.lon}&appid=f1fdda4864afff5226ddcc1a17f0350f`
+    console.log (weatherApiUrlForWeatherInfo);
+  }
+
+  fetch(weatherApiUrlForLonLat)
+    .then(functionForJSON)
+    .then(functionForApplication)
 }
 
 function searchForCityWeather (event) {
