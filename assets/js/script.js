3a00db1f8042e24126a10931cbe41ba158ee9edf
Dominika <domi_nika@hotmail.co.uk>
2021-04-07 10:07:00 +0100

    forecast weather function succesffuly called from get weather, unsuccessful build of html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,27 @@ function getCityName () {
   currentTarget.sibling("input").val();
 }
 
+function buildCurrentWeatherSection (cityName, currentWeatherObject) {
+ const article = $(".current-Weather").append(`
+  <h1>
+    Today's weather in CITY
+  </h1>
+  <div class="row">
+    <div class="col-lg-5 col-sm-12 p-3 currentWeather__Icon">
+      Icon here
+    </div>
+    <div class="col-lg-7 col-sm-12 p-3 currentWeather__Info ">
+      <ul class="list-group">
+        <li class="list-group-item currentWeather__Info--li">Temp</li>
+        <li class="list-group-item currentWeather__Info--li">Humidity</li>
+        <li class="list-group-item currentWeather__Info--li">Wind speed</li>
+        <li class="list-group-item currentWeather__Info--li">UV index</li>
+      </ul>
+    </div>`);
+
+  console.log(article)
+}
+
 function fetchWeatherData (cityName) {
   let weatherApiUrlForLonLat = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=f1fdda4864afff5226ddcc1a17f0350f`;
   
@@ -15,7 +36,7 @@ function fetchWeatherData (cityName) {
       lat: dataFromServer.coord.lat,
     };
 
-    weatherApiUrlForWeatherInfo = `https://api.openweathermap.org/data/2.5/onecall?lat=${lonLatObject.lat}&lon=${lonLatObject.lon}&appid=f1fdda4864afff5226ddcc1a17f0350f`
+    weatherApiUrlForWeatherInfo = `https://api.openweathermap.org/data/2.5/onecall?lat=${lonLatObject.lat}&lon=${lonLatObject.lon}&exclude=minutely,hourly&appid=f1fdda4864afff5226ddcc1a17f0350f`
 
     const functionForJSON = (responseObject) => {
       return responseObject.json();
@@ -27,15 +48,20 @@ function fetchWeatherData (cityName) {
         humidity: dataFromServer.current.humidity,
         windSpeed: dataFromServer.current.wind_speed,
         uvIndex: dataFromServer.current.uvi,
+        icon: dataFromServer.current.weather[0].icon,
       }
-      console.log(currentWeatherObject)
+      return currentWeatherObject;
+    }
+
+    const getForecastWeather = (dataFromServer) => {
+      console.log ("got it")
     }
 
     const functionForApplication = (dataFromServer) => {
       currentWeather = getCurrentWeather(dataFromServer);
+      forecastWeather = getForecastWeather(dataFromServer);
+      buildCurrentWeatherSection (cityName, currentWeather);
     }
-
-
 
     fetch(weatherApiUrlForWeatherInfo)
       .then(functionForJSON)
